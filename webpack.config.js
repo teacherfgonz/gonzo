e5a768341a0d52602d5308c12999b60ef156dd0e
Alejandro González <thytanium@gmail.com>
2021-11-07 18:43:00 -0300

    Removed postcss & postcss-loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ const isProduction = process.env.NODE_ENV === "production";
 const cssLoaders = [
   isProduction ? MiniCssExtractPlugin.loader : "style-loader",
   "css-loader",
-  "postcss-loader",
 ];
 
 module.exports = {
